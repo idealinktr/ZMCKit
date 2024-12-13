6167376aed9d8a7b3ac28aa88b0ea52c577bf533
can <ckocoglu@gmail.com>
2024-12-13 17:16:59 +0300

    sasas

--- Sources/ZMCKit/ZMCKit.swift
+++ Sources/ZMCKit/ZMCKit.swift
@@ -8,10 +8,12 @@
 import Foundation
 import UIKit
 
+#if !targetEnvironment(simulator)
 @_exported import SCSDKCameraKitReferenceUI
 @_exported import SCSDKCameraKit
 @_exported import SCSDKCoreKit
 @_exported import SCSDKCreativeKit
+#endif
 
 enum ZMCKitError: Error {
     case invalidPreviewView
